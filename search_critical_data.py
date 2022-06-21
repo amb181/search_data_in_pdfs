0bfeaecb8833dfd9e7de2243d385bc1a105a2346
Alejandro Molina <44465097+amb181@users.noreply.github.com>
2022-06-21 11:09:17 -0500

    Added GUI with exceptions

--- search_critical_data.py
+++ search_critical_data.py
@@ -1,41 +1,231 @@
 """ 
 Version 1.0.0
 almolina@tenaris.com
-09/2022
+06/2022
 """
+print("Starting app\nThis may take a few seconds...")
 # Import libraries
-from threading import Thread
 from tkinter import *
+from tkinter import filedialog
 import pandas as pd
-import os, glob, fitz, csv, time, itertools, shutil, sys
+import os, glob, fitz, csv, time, itertools, shutil, sys, datetime
+
+WINDOW_WIDTH = 450
+WINDOW_HEIGHT = 150
 
 class Window():
     def run(self):
+        self.search_init()
+
+    def search_init(self):
         self.master = Tk()
     
         self.master.title("Critical Data")
         self.master['background'] = "#499e03"
         self.master.iconbitmap(default=Window.resource_path("Tenaris_Logo.ico"))
-        Label(self.master, text="Files Location", bg='#499e03').grid(row=0, sticky=W)
-        
-        self.e1 = Entry(self.master)
-        self.e1.grid(row=0, column=1, sticky=EW)
-        
-        b = Button(self.master, text="Select", command=self.get_path)
-        b.grid(row=0, column=2,columnspan=2, rowspan=2, padx=5, pady=5)
+
+        self.l1 = Label(self.master, text="Folder location:", bg='#499e03', font=("Arial", 14))
+        self.l1.grid(row=0, column=0)
+
+        self.e1 = Entry(self.master, font=("Arial", 14))
+        self.e1.grid(row=0, column=1)
+
+        b = Button(self.master, text="Select", font=("Arial", 10), command=self.get_path)
+        b.grid(row=0, column=2)
+
+        self.master.columnconfigure(0, weight=1)
+        self.master.columnconfigure(1, weight=1)
+        self.master.columnconfigure(2, weight=1)
+        self.master.rowconfigure(0, weight=1)
+
         self.master.bind_all("<Key>", self._onKeyRelease, "+")
-        Window.center(self)
+        self.center(WINDOW_WIDTH, WINDOW_HEIGHT)
         self.master.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-        mainloop()
+        self.master.mainloop()
+    
+    def params_window(self):
+        self.master = Tk()
+
+        self.master.title("Critical Data")
+        self.master['background'] = "#499e03"
+        self.master.iconbitmap(default=Window.resource_path("Tenaris_Logo.ico"))
+
+        # Menu bar
+        menu_bar = Menu(self.master)
+        self.master.config(menu=menu_bar)
+        file_menu = Menu(menu_bar, tearoff=False)
+        file_menu.add_command(label="Change folder", command=self.search_init)
+        file_menu.add_separator()
+        file_menu.add_command(label="Exit", command=self.on_closing)
+        menu_bar.add_cascade(label="Config", menu=file_menu)
+
+        # Content
+        l1 = Label(self.master, text='Parameters (separate each with a ";") :', bg='#499e03', font=("Arial", 14))
+        l1.grid(row=0, columnspan=2)
+        
+        self.e1 = Entry(self.master, width=30, font=("Arial", 14))
+        self.e1.grid(row=1, column=0)
+        
+        b = Button(self.master, text="Search", font=("Arial", 10), command=self.get_params)
+        b.grid(row=1, column=1)
+
+        self.master.columnconfigure(0, weight=1)
+        self.master.columnconfigure(1, weight=1)
+        self.master.rowconfigure(0, weight=1)
+        self.master.rowconfigure(1, weight=1)
+
+        self.master.bind_all("<Key>", self._onKeyRelease, "+")
+        self.center(WINDOW_WIDTH, WINDOW_HEIGHT)
+        self.master.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+        self.master.mainloop()
+
+    def ask_filename(self):
+        self.master = Tk()
+        self.master.withdraw()
+
+        today = datetime.datetime.today().strftime('%Y-%m-%d').replace("-", "_")
+        global csv_name
+        csv_placeholder = "Search_report_" + today
+
+        csv_name = filedialog.asksaveasfilename(
+            defaultextension='.csv', 
+            filetypes=[('CSV files', '*.csv')],
+            initialdir=path,
+            initialfile = csv_placeholder
+        )
+
+        if ".csv" in csv_name:
+            csv_name = csv_name.replace(".csv", "")
+
+        self.master.destroy()
+
+    def waiting(self):
+        self.master = Tk()
+
+        self.master.title("Critical Data")
+        self.master['background'] = "#499e03"
+        gif = PhotoImage(file=Window.resource_path("loading.gif"))
+        Label(image=gif).grid(row=0, column=0)
+
+        self.center(340, 340)
+        self.master.protocol("WM_DELETE_WINDOW", self.on_closing)
+        
+        self.master.update()
+        
+    def done(self, time):
+        self.master.destroy()
+        self.master = Tk()
+        self.master.title("Critical Data")
+        self.master['background'] = "#499e03"
+
+        l1 = Label(self.master, text='Search done!', bg='#499e03', font=("Arial", 14))
+        l1.grid(row=0, columnspan=2)
+        l2 = Label(self.master, text='Elapsed time: {:.2f} min'.format(time), bg='#499e03', font=("Arial", 14))
+        l2.grid(row=1, columnspan=2)
+
+        b1 = Button(self.master, text="Open report", font=("Arial", 10), command=self.open_report)
+        b1.grid(row=2, column=0)
+        b2 = Button(self.master, text="Close", font=("Arial", 10), command=self.continue_search)
+        b2.grid(row=2, column=1)
+
+        self.master.columnconfigure(0, weight=1)
+        self.master.columnconfigure(1, weight=1)
+        self.master.rowconfigure(0, weight=1)
+        self.master.rowconfigure(1, weight=1)
+        self.master.rowconfigure(2, weight=1)
+        
+        self.center(WINDOW_WIDTH, WINDOW_HEIGHT)
+        self.master.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+        self.master.mainloop()
+
+    def continue_search(self):
+        self.master.destroy()
+        self.master = Tk()
+
+        self.master.title("Critical Data")
+        self.master['background'] = "#499e03"
+
+        global continue_flag
+        continue_flag = True
+
+        l1 = Label(self.master, text='Search again?', bg='#499e03', font=("Arial", 14))
+        l1.grid(row=0, columnspan=2)
+        
+        b1 = Button(self.master, text="Search!", font=("Arial", 12), command=self.master.destroy)
+        b1.grid(row=1, column=0)
+        b2 = Button(self.master, text="Exit", font=("Arial", 12), command=self.on_closing)
+        b2.grid(row=1, column=1)
+
+        self.master.columnconfigure(0, weight=1)
+        self.master.columnconfigure(1, weight=1)
+        self.master.rowconfigure(0, weight=1)
+        self.master.rowconfigure(1, weight=1)
+
+        self.center(WINDOW_WIDTH, WINDOW_HEIGHT)
+        self.master.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+        self.master.mainloop()
 
     def get_path(self):
         global path, folder
-        path = str(self.e1.get()) + "\\"
-        folder = glob.glob(path + "*.pdf")
+        path = str(self.e1.get())
+        if path == "":
+            path = filedialog.askdirectory()
+        if path == "":
+            self.master.destroy()
+            return self.search_init()
+        folder = glob.glob(path + "/*.pdf")
         self.master.destroy()
-        self.params_window()
-        
+
+        if path == "":
+            self.search_init()
+
+    def get_params(self):
+        global words_lookup
+        words_lookup = []
+        user_input = str(self.e1.get())
+        if user_input != "":
+            words_lookup= user_input.split(";")
+        else:
+            param_file = filedialog.askopenfilename(
+                filetypes=[('Excel files', '*.xlsx'), ('CSV files', '*.csv')],
+                initialdir=path
+            )
+
+            if param_file == "":
+                self.master.destroy()
+                return self.params_window()
+
+            if ".xlsx" in param_file:
+                df = pd.DataFrame(pd.read_excel(param_file))
+                words_lookup = df['Parametro'].tolist()
+            elif ".csv" in param_file:
+                df = pd.DataFrame(pd.read_csv(param_file))
+                words_lookup = df['Parametro'].tolist()
+            
+        for i in range(len(words_lookup)):
+            words_lookup[i] = words_lookup[i].strip().upper()
+            
+        self.master.destroy()
+
+    def open_report(self):
+        try:
+            os.system("start EXCEL.EXE " + csv_name + ".csv")
+        except Exception as e:
+            print(e.args)
+        self.continue_search()
+
+    def center(self, w_width, w_height):
+        screen_width = self.master.winfo_screenwidth() # 1920
+        screen_height = self.master.winfo_screenheight() # 1080
+        x_cordinate = int((screen_width/2) - (w_width/2))
+        y_cordinate = int((screen_height/2) - (w_height/2))
+
+        self.master.geometry("{}x{}+{}+{}".format(w_width, w_height, x_cordinate, y_cordinate))
+
     def _onKeyRelease(self, event):
         ctrl  = (event.state & 0x4) != 0
         if event.keycode==88 and  ctrl and event.keysym.lower() != "x": 
@@ -47,84 +237,11 @@ class Window():
         if event.keycode==67 and  ctrl and event.keysym.lower() != "c":
             event.widget.event_generate("<<Copy>>")
 
-    def params_window(self):
-        self.master = Tk()
-
-        self.master.title("Critical Data")
-        self.master['background'] = "#499e03"
-        self.master.iconbitmap(default=Window.resource_path("Tenaris_Logo.ico"))
-        Label(self.master, text='Parameters (separate each with a ";") :', bg='#499e03').grid(row=0, sticky=W)
-        
-        self.e1 = Entry(self.master)
-        self.e1.grid(row=0, column=1, sticky=EW)
-        
-        b = Button(self.master, text="Search", command=self.get_params)
-        b.grid(row=0, column=2,columnspan=2, rowspan=2, padx=5, pady=5)
-        self.master.bind_all("<Key>", self._onKeyRelease, "+")
-        Window.center(self)
-        self.master.protocol("WM_DELETE_WINDOW", self.on_closing)
-
-        mainloop()
-
-    def get_params(self):
-        global words_lookup
-        user_input = str(self.e1.get())
-        words_lookup= user_input.split(";")
-        for i in range(len(words_lookup)):
-            words_lookup[i] = words_lookup[i].strip().upper()
-        
-        self.master.destroy()
-
-    def waiting(self):
-        self.master = Tk()
-
-        self.master.title("Critical Data")
-        self.master['background'] = "#499e03"
-        gif = PhotoImage(file=Window.resource_path("loading.gif"))
-        Label(image=gif).grid(row=0, column=0)
-        Window.center(self)
-        self.master.protocol("WM_DELETE_WINDOW", self.on_closing)
-        
-        self.master.update()
-        
-    def done(self, time):
-        self.master.destroy()
-        self.master = Tk()
-
-        self.master.title("Critical Data")
-        self.master['background'] = "#499e03"
-
-        Label(self.master, text='Search done!', bg='#499e03').grid(row=0, sticky=EW)
-        Label(self.master, text='Elapsed time: {:.2f} min'.format(time), bg='#499e03').grid(row=1, sticky=EW)
-        
-        b1 = Button(self.master, text="Open report", command=self.open_report)
-        b1.grid(row=2, column=0, padx=5, pady=5)
-        b2 = Button(self.master, text="Close", command=self.master.destroy)
-        b2.grid(row=2, column=1, padx=5, pady=5)
-        Window.center(self)
-        self.master.protocol("WM_DELETE_WINDOW", self.master.destroy)
-
-        mainloop()
-
-    def open_report(self):
-        global report
-        report = True
-        self.master.destroy()
-
-    def center(self):
-        w = self.master.winfo_reqwidth()
-        h = self.master.winfo_reqheight()
-        ws = self.master.winfo_screenwidth()
-        hs = self.master.winfo_screenheight()
-        x = (ws/2) - (w/2)
-        y = (hs/2) - (h/2)
-        self.master.geometry('+%d+%d' % (x, y))
-
     def resource_path(relative_path):
         try:
             base_path = sys._MEIPASS
         except Exception:
-            base_path = os.path.abspath(".")
+            base_path = os.path.abspath("./imgs")
 
         return os.path.join(base_path, relative_path)
 
@@ -137,9 +254,9 @@ class Critical_Data():
     # Lookup critical data
     def search_data(self, pdf):
         # Create a copy of original file
-        os.makedirs(path + "highlighted_pdf\\", exist_ok=True)
-        shutil.copyfile(pdf, path + "highlighted_pdf\\" + os.path.basename(pdf))
-        searched_pdf = path + "highlighted_pdf\\" + os.path.basename(pdf)
+        os.makedirs(path + "\\highlighted_pdf\\", exist_ok=True)
+        shutil.copyfile(pdf, path + "\\highlighted_pdf\\" + os.path.basename(pdf))
+        searched_pdf = path + "\\highlighted_pdf\\" + os.path.basename(pdf)
         # Extract text and search parameter
         doc = fitz.open(searched_pdf)
         num_pag = doc.page_count
@@ -160,7 +277,7 @@ class Critical_Data():
 
     # Open excel file and append data
     def WriteCSV(self, data):
-        with open(path + "Search_report.csv", 'a+', encoding="utf-8") as csv_doc:
+        with open(csv_name + ".csv", 'a+', encoding="utf-8") as csv_doc:
             csv_doc = csv.writer(csv_doc)
             csv_doc.writerows(data)
 
@@ -173,59 +290,49 @@ if __name__ == "__main__":
     # Init classes
     app = Window()
     crit = Critical_Data()
-
     app.run()
-    global data
-    data = []
-    report = False
-    # Start csv file
-    headers = ['Document', 'Parameter', 'Page']
-    data.append(headers)
 
-    t = []
-    a = 0
+    while True:
+        global data
+        data = []
+        
+        # Start csv file
+        headers = ['Document', 'Parameter', 'Page']
+        data.append(headers)
 
-    start_time = time.time()
+        start_time = time.time()
 
-    # Waiting window
-    app.waiting()
+        # Ask for parameters to search
+        app.params_window()
+        # Ask name for csv
+        app.ask_filename()
+        # Waiting window
+        app.waiting()
+        
+        for file_ in folder:
+            crit.search_data(file_)
 
-    for file_ in folder:
-        t.append(Thread(target=crit.search_data, args=(file_,)))
-        t[a].start()
-        a += 1       
+        end_time = time.time() - start_time
+        end_time = end_time/60
 
-    for th in t:
-        th.join()
-
-    end_time = time.time() - start_time
-    end_time = end_time/60
-
-    # Delete previous report if exists
-    try:
-        os.remove(path + "Search_report.csv")
-    except:
-        pass
-
-    # Show done window
-    app.done(end_time)
-
-    # Open excel report
-    if report:
+        # Delete previous report if exists, without this data will be appended
         try:
-            os.system("start EXCEL.EXE " + path + "Search_report.csv")
-        except Exception as e:
-            print(e.args)
+            os.remove(csv_name + ".csv")
+        except:
+            pass
 
-    # Drop duplicates
-    data = list(data for data, _ in itertools.groupby(data))
-    crit.WriteCSV(data)
+        # Drop duplicates
+        data = list(data for data, _ in itertools.groupby(data))
+        crit.WriteCSV(data)
 
-    # Remove empty rows in excel file
-    df = pd.read_csv(path + "Search_report.csv", encoding="utf-8")
-    # Droping the empty rows
-    modifiedDF = df.dropna()
-    # Saving it to the csv file 
-    modifiedDF.to_csv(path + "Search_report.csv", index=False)
+        # Remove empty rows in excel file
+        df = pd.read_csv(csv_name + ".csv", encoding="utf-8")
+        # Droping the empty rows
+        modifiedDF = df.dropna()
+        # Saving it to the csv file 
+        modifiedDF.to_csv(csv_name + ".csv", index=False)
 
-    print("\nSearch finished in {} files.\nElapsed time: {:.2f} min".format(len(folder), end_time))
+        # Show done window
+        app.done(end_time)
+
+        print("\nSearch finished in {} files.\nElapsed time: {:.2f} min".format(len(folder), end_time))
